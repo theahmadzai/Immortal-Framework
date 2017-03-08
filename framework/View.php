749aab98d0c3aa66e9153d1c136ec75cbd154ac7
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-03-08 18:28:16 +0500

    Changed Path ../

--- framework/View.php
+++ framework/View.php
@@ -5,10 +5,10 @@ class View
 {
     public static function make($file, $data = [])
     {
-        $loader = new \Twig_Loader_Filesystem(APP_ROOT . '/../resources/views');
-        $twig   = new \Twig_Environment($loader); //['cache' => APP_ROOT . '/cache',]
+        $loader = new \Twig_Loader_Filesystem(APP_ROOT . '/resources/views');
+        $twig   = new \Twig_Environment($loader);//,['cache' => APP_ROOT . '/cache',]
 
-        $twig->addGlobal('ASSET_ROOT', ASSET_ROOT.'/../');
+        $twig->addGlobal('ASSET_ROOT', ASSET_ROOT.'../');
 
         try
         {
